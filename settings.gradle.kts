8d2db5dad2f07ab874c217d7e2a9c7367dbfb2b9
Jeonghyeon <jeonghyeon0208@g.hongik.ac.kr>
2024-12-09 11:23:24 +0900

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Recycrew"
+include(":app")
+include(":presentation")
+include(":data")
+include(":local")
+include(":remote")
+include(":feature")
+include(":domain")
